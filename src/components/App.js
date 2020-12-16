0c144ff02a549c2ab6a9b82e64da937aeec016a6
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2020-12-15 17:50:32 -0800

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="cards">
+        <div className="card">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
